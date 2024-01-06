ac736135eb0eede963238b0bc8bb0034af547b50
datwaft <datwaftx@gmail.com>
2024-01-06 11:50:32 -0600

    feat: close current pane with CMD+W and CTRL+SHIFT+w

--- wezterm.lua
+++ wezterm.lua
@@ -13,6 +13,12 @@ config.window_frame = {
 	-- Set font to SF Compact Text
 	font = wezterm.font("SF Compact Text"),
 }
+-- Close current pane instead of window with CMD+w and CTRL+SHIFT+w and do not ask for confirmation
+config.keys = {
+	{ key = "w", mods = "CMD", action = act.CloseCurrentPane({ confirm = false }) },
+	{ key = "w", mods = "CTRL|SHIFT", action = act.CloseCurrentPane({ confirm = false }) },
+	table.unpack(config.keys),
+}
 -- Set LEADER key
 config.leader = { key = "a", mods = "CMD", timeout_milliseconds = 1000 }
 -- Send LEADER key when pressing it twice
